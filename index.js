23f16bd3eca6945b55da5336bca2c8d4e3d65521
1bl4z3r <34515568+1bl4z3r@users.noreply.github.com>
2024-05-07 13:04:46 +0530

    Update UI

--- index.js
+++ index.js
@@ -1,77 +1,106 @@
-var showurl;
-$(function() {
-    $(':input[type="number"]').on('input change', function() {
-        if($("#phone").val().length >= 10)
-            $('#phone').css("border-color", "rgb(0, 255, 0)")
-        else
-        $('#phone').css("border-color", "rgb(255, 0, 0)")
-        if($("#ext").val().length >= 1)
-            $('#ext').css("border-color", "rgb(0, 255, 0)")
-        else
-            $('#ext').css("border-color", "rgb(255, 0, 0)")
-    });
-    $('#msgbox').on('input change', function() {
-        if($("#msgbox").val().length > 0)
-            $('#msgbox').css("border-color", "rgb(0, 255, 0)")
-        else
-            $('#msgbox').css("border-color", "rgb(255, 0, 0)")
-    });
+let textbox = document.getElementById('phoneno');
+let dropdown = document.getElementById('dropdown');
+let whtspp = document.getElementById('whatsapp');
+let telgrm = document.getElementById('telegram');
+let signal = document.getElementById('signal');
+let errord = document.getElementById('error');
+let errorh = document.getElementById('error-h4');
+let errorp = document.getElementById('error-body');
+let countryCodeList = [];
 
-});
+function extFinder(extNum) {
+  if (extNum.match(/\d/g) === null) {
+    return;
+  }
+  extNum = '+' + extNum.match(/\d/g).join("");
+  let selectIndex = 0;
+  let index = (extNum < 5) ? 5 : extNum.length;
+  for (index; index >= 2; index--) {
+    let r = `^\\${extNum.substring(0, index)}$`;
+    if (countryCodeList.findIndex(_strCheck) === -1) { selectIndex = 0; continue; }
+    else {
+      selectIndex = countryCodeList.findIndex(_strCheck) + 1;
+      break;
+    }
+    function _strCheck(el) {
 
-jQuery(document).on('paste', function(e) {
-    pasteData = e.originalEvent.clipboardData.getData('text')
-    if (pasteData) {
-        pasteData = pasteData.replace(/\D/g, '')
-        var split = pasteData.replace(/.{10}$/, ' $&').split(' ')
-        $("#ext").val(split[0]).change()
-        $("#phone").val(split[1]).change()
-        return false
+      return el.match(r);
     }
-});
-$('#country').change(function() {
-    $('#ext').val($('#country option:selected').val()).change()
-})
-$('#chkbx').change(
-    function() {
-        if(!(('#cpl').disable)){
-        $('#cpl').prop("disabled", true)
-        $('#cpl').css("color","rgba(255, 0, 0, 0.623)")}
-        if ($(this).is(':checked')){
-            $("#msgbox").prop('readonly', false)
-            $('#msgbox').css("border-color", "rgb(255, 0, 0)")
-        }else{
-            $("#msgbox").prop('readonly', true)
-            $('#msgbox').css("border-color", "")
-        } 
-        $("#msgbox").val('')
+  }
+  dropdown.selectedIndex = selectIndex;
+}
+
+function dropdownMake() {
+  fetch("./CountryCodes.json")
+    .then((res) => res.json())
+    .then((data) => {
+      // do stuff with the data
+      var CountryCodes = JSON.parse(JSON.stringify(data))
+      let text = "<option id='no-option' value=''>----Select your country----</option>"
+      for (let index in CountryCodes) {
+        text += "<option id=" + CountryCodes[index].country.replace(/\s+/g, '-').toLowerCase() + " value=" + CountryCodes[index].code.replace('-', '') + ">" + CountryCodes[index].country + "</option>";
+        countryCodeList.push(CountryCodes[index].code.replace('-', ''))
+      }
+      dropdown.innerHTML = text;
     });
-$('.submit').on('click', function() {
-    if($("#phone").val().length >=10 && $("#ext").val().length >= 1){
-        ext = $("#ext").val()
-        phone = $("#phone").val().slice(0, 10)
-        msg = $("#msgbox").val()
-        if ($('#chkbx').is(':checked') && msg!='') {
-            tail = ext + phone + "&text=" + msg
-            showurl = "https://wa.me/" + ext + phone +"?text="+msg
-        } else {
-            tail = ext + phone
-            showurl = "https://wa.me/" + ext + phone
-        }
-        wpurl = "https://api.whatsapp.com/send?phone=" + tail
-        $("#msgbox").prop('readonly', true)
-        $('#msgbox').css("border-color", "")
-        $("#msgbox").val("\n\nYour Shareable URL is : "+ showurl)
-        $("#chkbx").prop("checked", false)
-        $('#cpl').prop("disabled", false)
-        $('#cpl').css("color","rgba(0, 255, 0, 0.623)")
-        if (window.confirm('Redirect to WhatsApp?'))
-            window.open(wpurl, '_blank')
+}
+function dropdownSelect() {
+  var value = dropdown.value;
+  // var text = dropdown.options[dropdown.selectedIndex].text;
+  // console.log(text)
+  textbox.value = (value === '') ? '' : value.slice(1);
+}
+function handleLink(s){
+  errord.classList.add("hide");
+  if (textbox.value === '') {
+    errorh.innerHTML = "EMPTY FIELD";
+    errorp.innerHTML = "No phone number was entered. Please enter one";
+    errord.classList.remove("hide");
+  }
+  else if(textbox.value.match(/\d/g).join("").length < 8){
+    errorh.innerHTML = "INVALID PHONE NUMBER";
+    errorp.innerHTML = "Phone number received is less than 8 characters, which is invalid";
+    errord.classList.remove("hide");
+  }
+  else{
+    let ph = '+'+textbox.value.match(/\d/g).join("");
+    if (s === 'w'){
+      if (window.confirm('Redirect to WhatsApp?')) {
+        window.open('https://wa.me/'+ph,'_blank','noopener');
+      }
     }
-});
-$('#cpl').on('click',function(){
-    if (navigator.clipboard.writeText)
-        navigator.clipboard.writeText(showurl)
-    else
-        alert("Clipboard API is not supported")
+    else if(s === 't'){
+      if (window.confirm('Redirect to Telegram?')) {
+        window.open('https://t.me/'+ph,'_blank','noopener');
+      }
+    }
+    else if(s === 's'){
+      if (window.confirm('Redirect to Signal?')) {
+        window.open('https://signal.me/#p/'+ph,'_blank','noopener');
+      }
+    }
+    else{console.log("NONE")}
+  }
+}
+window.addEventListener("load", (e1) => {
+  dropdownMake();
+  textbox.addEventListener("paste", (e2) => {
+    e2.preventDefault();
+    let paste = (e2.clipboardData || window.clipboardData).getData("text");
+    textbox.value = paste;
+    extFinder(paste);
+  });
+  textbox.addEventListener("input", (e3) => {
+    e3.preventDefault();
+    if((textbox.value !== '')&&(textbox.value.match(/\d/g).join("").length > 2)&&(textbox.value.match(/\d/g).join("").length < 5)){
+      extFinder(textbox.value);
+    }
+    else if(textbox.value === ''){
+      dropdown.selectedIndex = 0;
+    }
+  });
+  dropdown.addEventListener("change", dropdownSelect);
+  telgrm.addEventListener("click", () => {setTimeout(handleLink,1000),'t'},false);
+  signal.addEventListener("click", () => {setTimeout(handleLink,1000),'s'},false);
+  whtspp.addEventListener("click", () => {setTimeout(handleLink,1000),'w'},false);
 });
